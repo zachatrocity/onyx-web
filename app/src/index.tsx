8a3390f8d86367fec0c35487d2ed42b6894c15c6
Luke Curley <kixelated@gmail.com>
2025-07-23 10:44:29 -0700

    Change the @ route again
    Former-commit-id: 1bbd1f8d4042aba49c678ed12ab24731afafd802

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -26,7 +26,7 @@ export function Hang(): JSX.Element {
 			<Router>
 				<Route path="/" component={About} />
 				<Route path="/account" component={() => <Account api={api} />} />
-				<Route path="/at/:name" component={() => <Sup canvas={canvas} api={api} room={useParams().name} />} />
+				<Route path="/demo/:name" component={() => <Sup canvas={canvas} api={api} room={useParams().name} />} />
 				<Route path="*" component={NotFound} />
 			</Router>
 		</>
